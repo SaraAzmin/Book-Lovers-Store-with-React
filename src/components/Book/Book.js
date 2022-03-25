43ddb6505a52e4a37f86269006a1e781eae689c8
SaraAzmin <azmin.sara17@gmail.com>
2022-03-25 20:03:23 +0100

    button added with icon

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Book.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Book = ({ book }) => {
     return (
-        <div className='border-2 rounded-lg border-blue-900'>
+        <div className='border-2 border-blue-900 relative'>
             <div className='flex justify-center my-4'>
                 <img className='w-2/5 ' src={book.picture} alt="" />
             </div>
             <h4 className='text-lg md:text-xl font-bold'>{book.name}</h4>
-            <h6 className='text-lg font-medium mt-2'>Price: ${book.price}</h6>
+            <h6 className='text-lg font-medium mt-2 mb-14'>Price: ${book.price}</h6>
+
+            <button className='bg-blue-800 text-white w-full absolute bottom-0 left-0 border-2 py-3 font-semibold'>Add to Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };

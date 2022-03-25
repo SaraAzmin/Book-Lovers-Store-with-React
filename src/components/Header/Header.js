16ab98a1270da7e0a04540c4210500d87e4dd3cf
SaraAzmin <azmin.sara17@gmail.com>
2022-03-25 18:00:49 +0100

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='bg-emerald-600'>
+            <h1 className='text-2xl lg:text-4xl font-semibold font-serif py-5 text-white'>Book Lovers Store</h1>
+        </div>
+    );
+};
+
+export default Header;
